8fcc63004fff38f9d5ce701179047ab035c0a04e
Josh Cheung <JTCheung97@gmail.com>
2024-01-17 15:50:26 -0800

    testing deployment

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -6,15 +6,24 @@ class Api::V1::BoardsController < ApplicationController
 
   def most_recent
     @boards = Board.order(created_at: :desc).first(10)
-    render json: @boards
+    if @boards
+      render json: @boards
+    else
+      render json: @board.errors
+    end
   end
 
   def all
     @boards = Board.all.order(created_at: :desc).page(params[:page])
-    render json: {
-      boards: @boards,
-      total_pages: @boards.total_pages,
-    }
+    if @boards
+      render json: {
+        boards: @boards,
+        total_pages: @boards.total_pages,
+      }
+    else
+      render json: @board.errors
+    end
+   
   end
 
   def create
