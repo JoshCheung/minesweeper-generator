4d91e6a2d50aec82e4772f43759b2989d24cab80
Josh Cheung <JTCheung97@gmail.com>
2024-01-17 15:44:40 -0800

    testing deployment

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::BoardsController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!, :only => "reply", :raise => false
+  # skip_before_action :verify_authenticity_token
+  # skip_before_action :authenticate_user!, :only => "reply", :raise => false
 
   before_action :set_board, only: %i[show]
 
