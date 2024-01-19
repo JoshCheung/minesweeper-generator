87a73d59448778d864daf4e1fd3b798f7edd751f
Josh Cheung <JTCheung97@gmail.com>
2024-01-18 16:35:05 -0800

    adding empty cell

---
+++ app/javascript/react/src/components/Board/EmptyCell.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './EmptyCell.css'
+const EmptyCell = () => {
+    return (
+        <div className="empty-shell">
+        </div>
+    )
+}
+export default EmptyCell;
